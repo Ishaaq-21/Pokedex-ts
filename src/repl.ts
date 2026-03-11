be4b0eb21115af6800e2b8dc695ab268a3822f5a
isaac <ishak.hadj14@gmail.com>
2026-03-11 23:54:15 +0100

    build the initial repl program that cleanInput and output the first word

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,27 @@
+import * as readline from "readline";
+
 export function cleanInput(input: string): string[] {
   return input.toLowerCase().trim().split(/\s+/).filter(Boolean);
+}
+
+export function startREPL() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+  rl.prompt();
+
+  rl.on("line", (input: string) => {
+    const wordsArr = cleanInput(input);
+
+    if (wordsArr.length === 0) {
+      rl.prompt();
+      return;
+    }
+
+    console.log(`Your command was: ${wordsArr[0]}`);
+
+    rl.prompt();
+  });
 }
