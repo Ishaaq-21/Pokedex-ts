8572d0dde7def14bff1a28cebcc169abd6207382
isaac <ishak.hadj14@gmail.com>
2026-03-13 00:37:27 +0100

    feat: add the help command logic

---
+++ src/commands/command_help.ts
@@ -0,0 +1,10 @@
+import { State } from "src/state.js";
+
+export function commandHelp(state: State) {
+  console.log("\nWelcome to the Pokedex!");
+  console.log("Usage:\n\n");
+  const { commands } = state;
+  for (const obj in commands) {
+    console.log(`\x1b[33m${obj}\x1b[0m: ${commands[obj].description}.`);
+  }
+}
