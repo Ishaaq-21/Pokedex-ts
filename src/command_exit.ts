502837405785f50660e7a2d72614580e99eb4366
isaac <ishak.hadj14@gmail.com>
2026-03-13 00:38:14 +0100

    change command_exit file location

--- src/command_exit.ts
+++
@@ -1,6 +0,0 @@
-import { CLICommand } from "./types/types.js";
-
-export function commandExit(commands: Record<string, CLICommand>) {
-  console.log("Closing the Pokedex... Goodby!");
-  process.exit(0);
-}
---
+++ src/commands/command_exit.ts
@@ -0,0 +1,8 @@
+import { State } from "src/state.js";
+
+export function commandExit(state: State) {
+  console.log("Closing the Pokedex... Goodbye!");
+  const { rl } = state;
+  rl.close();
+  process.exit(0);
+}
