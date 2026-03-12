ac04a4f95bbde5e154fd98320b02e332ca28a130
isaac <ishak.hadj14@gmail.com>
2026-03-13 00:39:27 +0100

    refactor: initialize a global state object and pass it to the repl function

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 // repl.js actually refers to repl.ts
 import { startREPL } from "./repl.js";
+import { initSTate } from "./state.js";
 
+const state = initSTate();
 function main() {
-  startREPL();
+  startREPL(state);
 }
 
 main();
