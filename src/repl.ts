f477538facc43cba37361edcf9f23721d703a68f
isaac <ishak.hadj14@gmail.com>
2026-03-13 00:40:31 +0100

    refactor: receive state variable as an argument instead of creating it inside the function

--- src/repl.ts
+++ src/repl.ts
@@ -1,18 +1,11 @@
-import * as readline from "readline";
-import { CLICommand } from "./types/types.js";
-import { getCommands } from "./commands_registry.js";
+import { State } from "./state.js";
 
 export function cleanInput(input: string): string[] {
   return input.toLowerCase().trim().split(/\s+/).filter(Boolean);
 }
 
-export function startREPL() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: "Pokedex > ",
-  });
-  rl.prompt();
+export function startREPL(state: State) {
+  const { rl, commands } = state;
 
   rl.on("line", (input: string) => {
     const wordsArr = cleanInput(input);
@@ -24,10 +17,14 @@ export function startREPL() {
 
     const command = wordsArr[0];
 
-    const commandsList: Record<string, CLICommand> = getCommands();
-
-    if (command in commandsList) {
-      commandsList[command].callback(commandsList);
+    if (command in commands) {
+      try {
+        commands[command].callback(state);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        }
+      }
     } else {
       console.log("Unknown command");
     }
