f1dae54f04d890e51704bc1617c511a253c09b8f
isaac <ishak.hadj14@gmail.com>
2026-03-15 03:46:25 +0100

    feat: add new command entry to commands record

--- src/commands/commands_registry.ts
+++ src/commands/commands_registry.ts
@@ -3,6 +3,7 @@ import { commandExit } from "./command_exit.js";
 import { commandHelp } from "./command_help.js";
 import { commandMap } from "./command_map.js";
 import { commandMapb } from "./command_mapb.js";
+import { commandExplore } from "./command_explore.js";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
@@ -25,6 +26,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "mapb",
       description: "Displays previous 20 areas",
       callback: commandMapb,
+    },
+    explore: {
+      name: "explore",
+      description: "Explores all pokemon in a given area",
+      callback: commandExplore,
     },
   };
 }
