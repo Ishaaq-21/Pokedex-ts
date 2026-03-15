5704f8cc9b5fcd66cd39d051e67879d87eca6e1d
isaac <ishak.hadj14@gmail.com>
2026-03-15 02:19:25 +0100

    use chalk to color the commands names

--- src/commands/command_help.ts
+++ src/commands/command_help.ts
@@ -1,10 +1,10 @@
 import { State } from "src/state.js";
-
+import chalk from "chalk";
 export async function commandHelp(state: State) {
   console.log("\nWelcome to the Pokedex!");
   console.log("Usage:\n\n");
   const { commands } = state;
   for (const obj in commands) {
-    console.log(`\x1b[33m${obj}\x1b[0m: ${commands[obj].description}.`);
+    console.log(`${chalk.yellow(`* ${obj}`)} : ${commands[obj].description}`);
   }
 }
