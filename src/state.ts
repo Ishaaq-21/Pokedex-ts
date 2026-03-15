18946180f41f4b0b041b944c1eafb3bf2cc8a493
isaac <ishak.hadj14@gmail.com>
2026-03-15 03:52:53 +0100

    update our callback signatures to accept optional args

--- src/state.ts
+++ src/state.ts
@@ -7,7 +7,7 @@ import chalk from "chalk";
 export type CLICommand = {
   name: string;
   description: string;
-  callback: (state: State) => Promise<void>;
+  callback: (state: State, ...args: string[]) => Promise<void>;
 };
 
 export type State = {
