ed9dc038c78052d90daa9277216f86b5cabbb4a8
isaac <ishak.hadj14@gmail.com>
2026-03-16 00:09:00 +0100

    feat: add the catch command to the commands registery

--- src/commands/commands_registry.ts
+++ src/commands/commands_registry.ts
@@ -4,6 +4,7 @@ import { commandHelp } from "./command_help.js";
 import { commandMap } from "./command_map.js";
 import { commandMapb } from "./command_mapb.js";
 import { commandExplore } from "./command_explore.js";
+import { commandCatch } from "./command_catch.js";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
@@ -31,6 +32,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "explore",
       description: "Explores all pokemon in a given area",
       callback: commandExplore,
+    },
+    catch: {
+      name: "catch",
+      description: "Tries catching pokemon",
+      callback: commandCatch,
     },
   };
 }
