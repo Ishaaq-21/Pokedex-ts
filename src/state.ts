df03aa902bc77fd5a0f838d19c559af63f3f9011
isaac <ishak.hadj14@gmail.com>
2026-03-16 00:22:59 +0100

    move types to a seperate dedicated file and import them

--- src/state.ts
+++ src/state.ts
@@ -1,22 +1,8 @@
-import { Interface } from "node:readline";
 import { getCommands } from "./commands/commands_registry.js";
 import * as readline from "readline";
 import { PokeAPI } from "./pokeapi.js";
 import chalk from "chalk";
-
-export type CLICommand = {
-  name: string;
-  description: string;
-  callback: (state: State, ...args: string[]) => Promise<void>;
-};
-
-export type State = {
-  commands: Record<string, CLICommand>;
-  rl: Interface;
-  pokeApi: PokeAPI;
-  nextLocationsURL: string;
-  prevLocationsURL: string;
-};
+import { State } from "./types/types.js";
 
 export function initSTate() {
   const rl = readline.createInterface({
